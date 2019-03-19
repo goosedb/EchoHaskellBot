2a3fc9c6fde6b0c808af1343ca1b90ac5e6fef95
DanilBerestov <ITDaun@yandex.ru>
2019-03-19 23:34:20 +0800

    parsing of config

--- src/Config.hs
+++ src/Config.hs
@@ -1,25 +1,54 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Config where
 
-import           Data.ByteString
-import           Data.Text
+import           Data.Aeson
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+import           Data.Text as T
+import           Data.Text.Encoding
+import           GHC.Generics
+import           Data.Aeson.Casing
 
 data Proxy = Proxy
-  { host :: ByteString
-  , port :: Int
-  }
+  { host :: !Text
+  , port :: !Int
+  } deriving (Show, Generic)
+
+instance FromJSON Proxy
+instance ToJSON Proxy
 
 data LogLevel
   = Debug
   | Warnings
   | Info
+  deriving (Show, Generic)
+
+instance FromJSON LogLevel
+instance ToJSON LogLevel
 
 data Service
   = Telegram
   | Slack
+  deriving (Show, Generic)
+
+instance FromJSON Service
+instance ToJSON Service
 
 data Config = Config
-  { token    :: Text
-  , proxy    :: Maybe Proxy
-  , logLevel :: LogLevel
-  , servise  :: Service
-  }
+  { token    :: !Text
+  , proxy    :: !(Maybe Proxy)
+  , logLevel :: !LogLevel
+  , service  :: !Service
+  } deriving (Show, Generic)
+
+instance FromJSON Config where
+  parseJSON = genericParseJSON $ aesonDrop 0 $ snakeCase
+instance ToJSON Config where
+  toJSON = genericToJSON $ aesonDrop 0 $ snakeCase
+
+loadConfig :: FilePath -> IO (Maybe Config)
+loadConfig path = do
+  cfg <- L.readFile path
+  return $ decode cfg 
