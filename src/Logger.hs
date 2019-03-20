884bc4a9a7c59582d1c0662653cfb88485089fa0
DanilBerestov <ITDaun@yandex.ru>
2019-03-20 13:34:19 +0800

    logger was rewritten

--- src/Logger.hs
+++ src/Logger.hs
@@ -1,10 +1,22 @@
 {-# LANGUAGE DeriveGeneric #-}
 
-module Logger where
+module Logger
+  ( createLogger
+  , writeLog
+  , LogLevel
+  ) where
 
 import           Data.Aeson
 import           GHC.Generics
 import           Prelude      hiding (log)
+
+data Logger =
+  Logger (LogLevel -> String -> String)
+         Output
+
+data Output
+  = Stdout
+  | File FilePath
 
 data LogLevel
   = Debug
@@ -21,11 +33,16 @@ instance FromJSON LogLevel
 
 instance ToJSON LogLevel
 
-createLogger :: LogLevel -> LogLevel -> String -> IO ()
-createLogger loggerLogLevel = log
+createLogger :: String -> LogLevel -> Logger
+createLogger out loggerLevel = Logger logger (stream out)
   where
-    log :: LogLevel -> String -> IO ()
-    log logLevel message =
-      if logLevel >= loggerLogLevel
-        then putStrLn $ show logLevel ++ message
-        else return ()
+    logger logLevel message =
+      if logLevel >= loggerLevel
+        then show logLevel ++ message ++ "\n"
+        else []
+    stream "STDOUT" = Stdout
+    stream _        = File out
+
+writeLog :: Logger -> LogLevel -> String -> IO ()
+writeLog (Logger lgr (File path)) lvl msg = appendFile path $ lgr lvl msg
+writeLog (Logger lgr Stdout) lvl msg      = putStr $ lgr lvl msg
